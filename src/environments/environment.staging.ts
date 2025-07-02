80f49fe22811bb6ade11e8ab6e67659974df9ba9
Prabakaran <prabakar1308@gmail.com>
2025-07-02 17:25:14 +0530

    update staging api url

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  HOST: 'https://gms-api-qa-374265160517.asia-south2.run.app/api', // Replace with your actual API URL
+  HOST: 'https://gms-api-dev-374265160517.asia-south1.run.app/api', // Replace with your actual API URL
 };
